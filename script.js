ace871252fa1743c3bf1f6d588a27d430eff4c38
benedikturcan <benedikt_urcan@hotmail.de>
2023-02-18 18:28:16 +0100

    adding lection 136 code

--- script.js
+++ script.js
@@ -85,44 +85,123 @@
 // const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 // greetArr('Hi')('Benedikt');
 
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNum}`
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
+// lufthansa.book(234, 'Benedikt');
+// console.log(lufthansa);
+
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
+
+// // call methode
+// const book = lufthansa.book;
+
+// book.call(eurowings, 23, 'Benedikt Urcan');
+// console.log(eurowings);
+
+// book.call(lufthansa, 56, 'Hagebert Lellek');
+// console.log(lufthansa);
+
+// const swissair = {
+//   airline: 'Swiss Airline',
+//   iataCode: 'SA',
+//   bookings: [],
+// };
+
+// book.call(swissair, 95, 'Angela Merkel');
+
+// // // Apply methode
+// // const flightData = [583, 'Benedikt Urcan'];
+// // book.apply(swiss, flightData);
+// // console.log(swiss);
+
+// // Bind methode
+// const bookEW = book.bind(eurowings);
+// const bookLH = book.bind(lufthansa);
+
+// bookEW(53, 'Lara Crof');
+
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('Benedikt Urcan');
+// bookEW23('Matha Coopper');
+
+// // Event listeners examples
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   console.log(this);
+
+//   this.planes++;
+//   console.log(this.planes);
+// };
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// // Partial Application
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.23);
+
+// console.log(addVAT(100));
+// console.log(addVAT(23));
+
+// const addTaxRate = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
+//   };
+// };
+// const addVAT2 = addTax(0.23);
+
+// console.log(addVAT(100));
+// console.log(addVAT(23));
+
+// Coding challenge
+const poll = {
+  question: 'What is your fav programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+
+  answers: new Array(4).fill(0),
+  registerNewAnwer() {
+    // Get the answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
     );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+    console.log(answer);
+    //register or update the answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type == 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
   },
 };
-lufthansa.book(234, 'Benedikt');
-console.log(lufthansa);
+poll.registerNewAnwer();
 
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnwer.bind(poll));
 
-// call methode
-const book = lufthansa.book;
-
-book.call(eurowings, 23, 'Benedikt Urcan');
-console.log(eurowings);
-
-book.call(lufthansa, 56, 'Hagebert Lellek');
-console.log(lufthansa);
-
-const swissair = {
-  airline: 'Swiss Airline',
-  iataCode: 'SA',
-  bookings: [],
-};
-
-book.call(swissair, 95, 'Angela Merkel');
-
-// Apply methode
-const flightData = [583, 'Benedikt Urcan'];
-book.apply(swiss, flightData);
-console.log(swiss);
+poll.displayResults.call({ answers: [5, 4, 3] }, 'string');
